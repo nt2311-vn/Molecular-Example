e1cf857f6f9a81f1c3d88b557c830bae6745784f
nt2311-vn <nguyentoan231196@gmail.com>
2024-05-31 00:22:22 +0700

    implement auth service to main

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import UserService from "./services/user.service.js";
 import EmailService from "./services/email.service.js";
+import AuthService from "./services/auth.service.js";
 
 async function startApp() {
 	await UserService.start();
 	await EmailService.start();
+	await AuthService.start();
 
 	try {
 		const newUser = await UserService.call("user.createUser", {
@@ -23,10 +25,21 @@ async function startApp() {
 			content: "Thank you for signing up",
 		});
 		console.log(emailResult);
+
+		// Simulate auth
+
+		const authResult = await AuthService.call("auth.authUser", {
+			username: newUser.username,
+			password: "password",
+		});
+
+		console.log("Auth result: ", authResult);
 	} catch (err) {
 		console.log("Error: ", err);
 	} finally {
 		await UserService.stop();
+		await EmailService.stop();
+		await AuthService.stop();
 	}
 }
 
